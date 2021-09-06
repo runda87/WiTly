7e10f80febbf6ca21c3332e7aae77d3f31f189e0
runda-git <84775206+runda-git@users.noreply.github.com>
2021-09-06 13:23:14 +0800

    styling pages

---
+++ fixtures/users.js
@@ -0,0 +1,13 @@
+export const peopleData = [{
+    id: 1,
+    level: 'Admin',
+    username: '',
+    password: '',
+},
+{
+    id: 2,
+    level: 'viewer',
+    username: '',
+    password: '',
+}
+]
--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 import express from 'express';
+import { peopleData } from './fixtures/people.js';
+
 
 const app = express();
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
+app.set('views', './views/pages/');
+app.use( express.static( 'public' ) );
 
 app.listen(3000, () => {
     console.log('server started!')
 });
 
 app.get('/', (req,res) => {
-    res.send('Hello, World!')
+    // res.send('Hello, World!')
+    console.log(peopleData);
+    res.render('index', { people: peopleData});
 });
+
+app.get('/profile/:id', (req, res) => {
+    let personId = req.params.id;
+    let person;
+    peopleData.forEach((personData) => {
+        if (personData.id == personId) {
+            person = personData;
+        }
+    })
+    console.log(person);
+    res.render('profile', { person: person});
+})
+
+app.get('/', (req,res) => {
+    console.log(usersData);
+    res.render('index', { login: usersData});
+});
+
+app.get('/login/:id', (req, res) => {
+    let personId = req.params.id;
+    let users;
+    userData.forEach((usersData) => {
+        if (usersData.id == personId) {
+            person = usersData;
+        }
+    })
+    console.log(users);
+    res.render('login', { users: users});
+})
+
+app.get('/loginform', function(req, res, next) { 
+    res.render('login', { title: 'Login Page', message }); 
+ }); 
+
+ app.get('/userprofile',  function(req, res, next) {
+    res.render('profile', { title: 'Profile Page', user : req.user,
+    avatar: gravatar.url(req.user.email ,  {s: '100', r: 'x', d:
+     'retro'}, true) });
+});
+
+app.get('/signup', function(req,res, next) {
+    res.render('signup', { titile: 'Sign up', });
+});
+
